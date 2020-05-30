15600b8ac2aaabed7f56617c70690938df8f573b
HafizZiq <hafizuddinismail552@gmail.com>
2020-05-30 18:33:22 +0800

    kerbun

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -1,73 +1,59 @@
+"""@CarbonNowShBot for @UniBorg
 """
-Carbon Scraper Plugin for Userbot. //text in creative way.
-usage: .carbon //as a reply to any text message
 
-Thanks to @AvinashReddy3108 for a Base Plugin.
-Go and Do a star on his repo: https://github.com/AvinashReddy3108/PaperplaneExtended/
-
-"""
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 from telethon import events
-from urllib.parse import quote_plus
-from urllib.error import HTTPError
-from time import sleep
-import asyncio
-import os
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from uniborg.util import admin_cmd
+import random
 
-@borg.on(events.NewMessage(pattern=r"\.carbon", outgoing=True))
-async def carbon_api(e):
- if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-   """ A Wrapper for carbon.now.sh """
-   await e.edit("Processing...")
-   CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
-   CARBONLANG = "en"
-   textx = await e.get_reply_message()
-   pcode = e.text
-   if pcode[8:]:
-         pcode = str(pcode[8:])
-   elif textx:
-         pcode = str(textx.message) # Importing message to module
-   code = quote_plus(pcode) # Converting to urlencoded
-   url = CARBON.format(code=code, lang=CARBONLANG)
-   chrome_options = Options()
-   chrome_options.add_argument("--headless")
-   chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
-   chrome_options.add_argument("--window-size=1920x1080")
-   chrome_options.add_argument("--disable-dev-shm-usage")
-   chrome_options.add_argument("--no-sandbox")
-   chrome_options.add_argument('--disable-gpu')
-   prefs = {'download.default_directory' : './'}
-   chrome_options.add_experimental_option('prefs', prefs)
-   await e.edit("Processing 30%")
 
-   driver = webdriver.Chrome(executable_path=Config.CHROME_DRIVER, options=chrome_options)
-   driver.get(url)
-   download_path = './'
-   driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-   params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
-   command_result = driver.execute("send_command", params)
+@borg.on(admin_cmd(pattern="carbon ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    #
+    input_str = event.pattern_match.group(1)
+    to_rip_mesg = event
+    if event.reply_to_msg_id and (not input_str or input_str == "reply"):
+        rep_mesg = await event.get_reply_message()
+        input_str = rep_mesg.message
+        to_rip_mesg = rep_mesg
+    #
+    chat = "@CarbonNowShBot"
+    chat_e = await event.client.get_entity(chat)
+    #
+    await event.edit("creating a carbon")
+    async with event.client.conversation(chat_e, timeout=180) as conv:
+        try:
+            await conv.send_message(input_str)
+            response = await conv.wait_event(events.MessageEdited(
+                incoming=True,
+                from_users=chat_e.id
+            ))
+            # await conv.send_read_acknowledge(response)
+            # we got a response with buttons
+            # decide to click on a random button
+            row = random.randint(0, 8)
+            logger.info(row)
+            column = random.randint(0, 2)
+            logger.info(column)
+            #
+            await response.click(row, column)
+            response = await conv.wait_event(events.NewMessage(
+                incoming=True,
+                from_users=chat_e.id
+            ))
+            response_caption = response.message.message
+            response_caption_sp = response_caption.split("\n")
+            # the above string has channel usernames,
+            # remove the useless advertisements
+            response_caption = "\n".join(response_caption_sp[0:2])
+            carbon_media = response.message.media
+            await to_rip_mesg.reply(response_caption, file=carbon_media)
+            # cleaning up
+            # await conv.send_read_acknowledge(response)
+            await event.delete()
+        except YouBlockedUserError:
+            await event.reply("Please unblock me (@CarbonNowShBot)")
+            return
 
-   driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
-   sleep(5) # this might take a bit.
-   driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-   sleep(5)
-   await e.edit("Processing 50%")
-   driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-   sleep(5) #Waiting for downloading
-
-   await e.edit("Processing 90%")
-   file = './carbon.png'
-   await e.edit("Done!!")
-   await e.client.send_file(
-         e.chat_id,
-         file,
-         caption="Carbon by [Dawn Labs](https://dawnlabs.io)/",
-         force_document=True,
-         reply_to=e.message.reply_to_msg_id,
-         )
-
-   os.remove('./carbon.png')
-   # Removing carbon.png after uploading
-   await e.delete() # Deleting msg
